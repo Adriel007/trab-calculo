f579dc50dfa90d3de80302d4f021aac3bd074760
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-11-20 12:52:32 -0300

    reg logistica funcionando, com arquivo bash de treinamento com diferentes lr e txt com resultados

--- logistic-regression.py
+++ logistic-regression.py
@@ -1,53 +1,16 @@
 import matplotlib.pyplot as plt
 import math as math
-import numpy as np
-import random
-from PIL import Image
+import time
+import sys
 
-debugger = True
-
-if debugger:
-    file = open('debug.txt', 'w')
-    file.write("")
-    file.close()
-
-def debug(localsVar):
-    if debugger:
-        file = open('debug.txt', 'a')
-        text = str(localsVar).split(", ")
-        oldText = open('debug.txt', 'r').read()
-        for i in range(len(text)):
-            if text[i] not in oldText:
-                file.write(text[i] + ", ")
-        file.write("\n")
-        file.close()
-
-paths = {
-    'test': {
-        'path': './test/bears/',
-        'length': 8
-    },
-    'training': {
-        'path': './training/bears/',
-        'length': 60
-    },
-    'pandas': {
-        'path': './training/pandas/',
-        'length': 3
-    },
-    'keys': {
-        'polar': 'polar_bear',
-        'black': 'black',
-        'panda': 'panda'
-    }
-}
-
-def plot_points(datasetx, datasety, a, b, c, xlabel="X", ylabel="Y", title="Graph with Sigmoid"):
-    plt.scatter(datasetx, datasety, color='blue', label="Data Points")
+def plot_points(datasetx, datasety, tsx, tsy, a, b, c, xlabel="X", ylabel="Y", title="Graph with Sigmoid"):
+    plt.scatter(datasetx, datasety, color='blue', label="Training Data")
 
     sigmoid_values = [sigmoid(a * x**2 + b * x + c) for x in datasetx]
 
     plt.plot(datasetx, sigmoid_values, color='red', label="Sigmoid Line")
+
+    plt.scatter(tsx, tsy, color='green', label="Predictions Data")
 
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -55,235 +18,148 @@ def plot_points(datasetx, datasety, a, b, c, xlabel="X", ylabel="Y", title="Grap
     plt.legend()
     plt.grid(True)
     plt.show()
-###################################### TRATAMENTO DE IMAGEM ###############################################
-
-def get_images(path_object, key):
-    images = []
-    for i in range(1, path_object['length'] + 1):
-        images.append(path_object['path'] + key + '/' + key + str(i) + '.jpg')
-    return images
-
-def img2gray(matrix_RGB):
-    for i in range(len(matrix_RGB)):
-        for j in range(len(matrix_RGB[0])):
-            # Modifica pixel para escala de cinza por meio de média aritmética
-            matrix_RGB[i][j] = (matrix_RGB[i][j][0] + matrix_RGB[i][j][1] + matrix_RGB[i][j][2]) // 3
-    return matrix_RGB 
-
-def resize_image(matrix_BW, new_height, new_width):
-    old_height = len(matrix_BW)
-    old_width = len(matrix_BW[0])
-
-    # Matrix resultado vazia com o tamanho correto
-    resized_image = [[0 for _ in range(new_width)] for _ in range(new_height)]
-
-    # Razões entre escalas
-    row_scale = old_height // new_height
-    col_scale = old_width // new_width
-
-    for i in range(new_height):
-        for j in range(new_width):
-            # Mapeia a posição do pixel novo para o do pixel antigo
-            old_i = i * row_scale
-            old_j = j * col_scale
-
-            # Interpolação bilinear simples
-            top_left_i = int(old_i)
-            top_left_j = int(old_j)
-
-            # Verifica limites da imagem original
-            if top_left_i >= old_height - 1:
-                top_left_i = old_height - 2
-            if top_left_j >= old_width - 1:
-                top_left_j = old_width - 2
-
-            bottom_right_i = top_left_i + 1
-            bottom_right_j = top_left_j + 1
-
-            # Fração para interpolação
-            delta_i = old_i - top_left_i
-            delta_j = old_j - top_left_j
-
-            # Atribui os 4 vizinhos
-            top_left = matrix_BW[top_left_i][top_left_j]
-            top_right = matrix_BW[top_left_i][bottom_right_j]
-            bottom_left = matrix_BW[bottom_right_i][top_left_j]
-            bottom_right = matrix_BW[bottom_right_i][bottom_right_j]
-
-            # Interpolação bilinear
-            top = (1 - delta_j) * top_left + delta_j * top_right
-            bottom = (1 - delta_j) * bottom_left + delta_j * bottom_right
-            value = (1 - delta_i) * top + delta_i * bottom
-
-            # Atribui o valor interpolado ao pixel novo
-            resized_image[i][j] = value
-
-    return resized_image
-
-def img2matrix(image_path):
-    img = Image.open(image_path)
-    img = img.convert('RGB')
-    width, height = img.size
-
-    rgb_matrix = []
-
-    for y in range(height):
-        row = []
-        for x in range(width):
-            # Obtém o valor RGB de cada pixel
-            r, g, b = img.getpixel((x, y))
-            row.append((r, g, b))
-        rgb_matrix.append(row)
-
-    return rgb_matrix
-
-def matrix2img(matrix_BW, filename):
-    # Dimensões da matriz
-    height = len(matrix_BW)
-    width = len(matrix_BW[0])
-
-    # Cria uma nova imagem em tons de cinza (mode 'L' é grayscale)
-    img = Image.new('L', (width, height))
-
-    # Preenche a imagem com os valores da matriz
-    for y in range(height):
-        for x in range(width):
-            img.putpixel((x, y), matrix_BW[y][x])
-
-    img.save(filename)
-    return True
-
-def matrix2vec(matrix_BW):
-    vector = []
-    for i in range(len(matrix_BW)):
-        for j in range(len(matrix_BW[0])):
-            vector.append(matrix_BW[i][j])
-    return vector
 
 ######################################### CALCULO ###################################################
+def grad_a(a, b, c, x, y):
+    z = a * x**2 + b * x + c
+    sigmoid_z = sigmoid(z)
+    return -(y - sigmoid_z) * sigmoid_z * (1 - sigmoid_z) * x**2
+
+def grad_b(a, b, c, x, y):
+    z = a * x**2 + b * x + c
+    sigmoid_z = sigmoid(z)
+    return -(y - sigmoid_z) * sigmoid_z * (1 - sigmoid_z) * x
+
+def grad_c(a, b, c, x, y):
+    z = a * x**2 + b * x + c
+    sigmoid_z = sigmoid(z)
+    return -(y - sigmoid_z) * sigmoid_z * (1 - sigmoid_z)
+
+def parabolica(a, b, c, xn, potencia):
+    z = a * xn**2 + b * xn + c
+    sigmoid_z = sigmoid(z)
+    return sigmoid_z * (1 - sigmoid_z) * xn**potencia
+
 def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
+  return 1/(1+math.exp(-x)) 
 
-# Gradientes
-def gradDS2(datasetx, datasety, a, b, c):
-    grad_a = 0
-    grad_b = 0
-    grad_c = 0
+
+def gradDS(datasetx, datasety, a, b, c):
+    grad_a_sum = 0
+    grad_b_sum = 0
+    grad_c_sum = 0
 
     for i in range(len(datasetx)):
-        pred = a * datasetx[i]**2 + b * datasetx[i] + c
-        grad_a += 2 * (pred - datasety[i]) * (datasetx[i]**2)
-        grad_b += 2 * (pred - datasety[i]) * (datasetx[i])
-        grad_c += 2 * (pred - datasety[i])
+        grad_a_sum += grad_a(a, b, c, datasetx[i], datasety[i])
+        grad_b_sum += grad_b(a, b, c, datasetx[i], datasety[i])
+        grad_c_sum += grad_c(a, b, c, datasetx[i], datasety[i])
 
-    return (grad_a, grad_b, grad_c)
+    return grad_a_sum, grad_b_sum, grad_c_sum
 
-# Funções de a, b e c
-def fa_quad(a, b, c, xn):
-    exp = -(a*xn**2 + b*xn + c)
-    exp_term = math.exp(exp)
-    result = xn**2 * exp_term / (1 + exp_term)**2
-    print (exp, exp_term, result)
-    return result
+def dist2_parabolica(a_0, b_0, c_0, a_n, b_n, c_n):
+    return ((a_n - a_0)**2 + (b_n - b_0)**2 + (c_n - c_0)**2)**0.5
 
-def fb_quad(a, b, c, xn):
-    exp = -(a*xn**2 + b*xn + c)
-    print (exp)
-    exp_term = math.exp(exp)
-    result = xn * exp_term / (1 + exp_term)**2
-    return result
-
-def fc_quad(a, b, c, xn):
-    exp = -(a*xn**2 + b*xn + c)
-    print (exp)
-    exp_term = math.exp(exp)
-    result = exp_term / (1 + exp_term)**2
-    return result
-
-# Gradientes
-def gradDS_logistic_quad(datasetx, datasety, a, b, c):
-    grad_a = 0
-    grad_b = 0
-    grad_c = 0
-    for i in range(len(datasetx)):
-        pred = a * datasetx[i]**2 + b * datasetx[i] + c
-        grad_a += 2 * (datasety[i] - pred) * fa_quad(a, b, c, datasetx[i])
-        grad_b += 2 * (datasety[i] - pred) * fb_quad(a, b, c, datasetx[i])
-        grad_c += 2 * (datasety[i] - pred) * fc_quad(a, b, c, datasetx[i])
-        debug(locals())
-
-    return (grad_a, grad_b, grad_c)
-
-def dist2(x_n, y_n, z_n, x_0, y_0, z_0):
-    return ((x_n - x_0)**2 + (y_n - y_0)**2 + (z_n - z_0)**2)**0.5
-
-def gradDS_quad(datasetx, datasety, a_0, b_0, c_0, tol, lr):
-    a_n = a_0
-    b_n = b_0
-    c_n = c_0
+def gradienteDescendenteParabolica(X, Y, a_0, b_0, c_0, tol, lr):
+    # Coeficientes iniciais
+    a_n, b_n, c_n = a_0, b_0, c_0
     
-    a_n1 = 99999999
-    b_n1 = 99999999
-    c_n1 = 99999999
+    # Próximos coeficientes
+    a_n1, b_n1, c_n1 = [99999999] * 3
     
+    # Contador de iterações
     i = 0
-
+    
     while True:
-        grad_a, grad_b, grad_c = gradDS_logistic_quad(datasetx, datasety, a_n, b_n, c_n)
+        # Gradientes
+        grad_a, grad_b, grad_c = gradDS(X, Y, a_n, b_n, c_n)
+        
+        # Atualizar coeficientes usando gradiente descendente
         a_n1 = a_n - lr * grad_a
         b_n1 = b_n - lr * grad_b
         c_n1 = c_n - lr * grad_c
+        
+        # Incrementar contador de iterações
         i += 1
         
-        if dist2(a_n1, b_n1, c_n1, a_n, b_n, c_n) <= tol:
-            break
+        # Calcular o erro (distância entre os pontos anteriores e os novos)
+        err = dist2_parabolica(a_n, b_n, c_n, a_n1, b_n1, c_n1)
+        
+        if err > tol:
+            # Atualizar os coeficientes para a próxima iteração
+            a_n, b_n, c_n = a_n1, b_n1, c_n1
         else:
-            a_n = a_n1
-            b_n = b_n1
-            c_n = c_n1
+            # Parar quando o erro for menor que a tolerância
+            break
+    
+    return i, a_n, b_n, c_n
 
-    return (i, a_n, b_n, c_n)
+def predictY(x, a, b, c):
+    return sigmoid(a * x**2 + b * x + c)
+
+################################## MÉTRICAS #############################################
+
+def precision(y_true, y_pred):
+    true_positives = sum(yt == 1 and yp == 1 for yt, yp in zip(y_true, y_pred))
+    false_positives = sum(yt == 0 and yp == 1 for yt, yp in zip(y_true, y_pred))
+    return true_positives / (true_positives + false_positives) if (true_positives + false_positives) > 0 else 0
+
+def recall(y_true, y_pred):
+    true_positives = sum(yt == 1 and yp == 1 for yt, yp in zip(y_true, y_pred))
+    false_negatives = sum(yt == 1 and yp == 0 for yt, yp in zip(y_true, y_pred))
+    return true_positives / (true_positives + false_negatives) if (true_positives + false_negatives) > 0 else 0
+
+def f1score(y_true, y_pred):
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    return 2 * (prec * rec) / (prec + rec) if (prec + rec) > 0 else 0
+
+def accuracy(y_true, y_pred):
+    correct = sum(yt == yp for yt, yp in zip(y_true, y_pred))
+    return correct / len(y_true)
 
 ################################### USE #################################################
 
 
 ########### TRAINING ###########
-x_list = []
-y_list = []
+train_x = []
+train_y = []
 
-polar = get_images(paths['training'], paths['keys']['polar'])
-black = get_images(paths['training'], paths['keys']['black'])
+for i in range(-10, 10, 1):
+    train_x.append(i)
+    train_y.append(0)
 
-# 2 FORS PARA DEIXAR ORGANIZADO
+for i in range(20, 40, 1):
+    train_x.append(i)
+    train_y.append(1)
 
-for i in range(paths['training']['length']):
-    black_matrix = img2matrix(black[i])
-    black_matrix = resize_image(img2gray(black_matrix), 64, 64)
-    black_matrix = matrix2vec(black_matrix)
-    black_simplified = sum(black_matrix) // len(black_matrix) # SIMPLIFICAÇÃO POR MÉDIA ( RECOMENDADO SERIA ALGO COMO PCA )
-    x_list.append(black_simplified)
-    y_list.append(0)
-
-for i in range(paths['training']['length']):
-    polar_matrix = img2matrix(polar[i])
-    polar_matrix = resize_image(img2gray(polar_matrix), 64, 64)
-    polar_matrix = matrix2vec(polar_matrix)
-    polar_simplified = sum(polar_matrix) // len(polar_matrix) # SIMPLIFICAÇÃO POR MÉDIA ( RECOMENDADO SERIA ALGO COMO PCA )
-    x_list.append(polar_simplified)
-    y_list.append(1)
-
-a_0 = random.uniform(min(x_list), max(x_list)) # Dados aleatorios proximos ao valores do dataset    
-b_0 = random.uniform(min(x_list), max(x_list)) # Dados aleatorios proximos ao valores do dataset    
-c_0 = random.uniform(min(x_list), max(x_list)) # Dados aleatorios proximos ao valores do dataset    
-learning_rate = 0.01
+a_0 = 0.1
+b_0 = 0.1
+c_0 = 0.1
+learning_rate = float(sys.argv[1])
+isBashTest = sys.argv[2]
 tolerance = 1e-6
-    
-iterations, a_opt, b_opt, c_opt = gradDS_quad(x_list, y_list, a_0, b_0, c_0, tolerance, learning_rate)
-    
-print (x_list)
 
-plot_points(x_list, y_list, a_opt, b_opt, c_opt, "X", "Y", "Graph of Points")
+print(f"Learning Rate: {learning_rate} | Tolerance: {tolerance}")
 
+start = time.time()
 
-# Mostrar resultado final do pre-processamento de imagem:
-# matrix2img(resize_image(img2gray(img2matrix(get_images(paths['training'], paths['keys']['polar'])[0])), 64, 64), 'exemplo.jpg')
+iterations, a_opt, b_opt, c_opt = gradienteDescendenteParabolica(train_x, train_y, a_0, b_0, c_0, tolerance, learning_rate)
+
+########### TESTING ###########
+test_x = [0, 3.5, 6.6, 11.5, 17.8, 20]
+test_y = []
+
+for x in test_x:
+    test_y.append(predictY(x, a_opt, b_opt, c_opt))
+
+f1 = f1score(train_y, test_y)
+acc = accuracy(train_y, test_y)
+
+end = time.time()
+print(f"Tempo de execução: {end - start}")
+
+print(f"Iterações: {iterations} | a: {a_opt} | b: {b_opt} | c: {c_opt}")
+print(f"F1 Score: {f1} | Accuracy: {acc}")
+
+if isBashTest == "False":
+    plot_points(train_x, train_y, test_x, test_y, a_opt, b_opt, c_opt, "X", "Y", "Graph of Points")
--- readme.md
+++ readme.md
@@ -17,14 +17,11 @@ Regressão linear de 2º grau e gradiente descendente
 ## Regressão Logística:
 ![Imagem de um gráfico de regressão logística](https://www.hashtagtreinamentos.com/wp-content/uploads/2022/12/REGRESSAO-LOGISTICA-3.png)
 
-### Dataset:
-Utilizou-se de um dataset encontrado no Kaggle de imagens de ursos, somente algumas imagens de ursos polares, negros e pandas foram utilizadas. O dataset original pode ser acessado clicando [aqui](https://www.kaggle.com/datasets/hoturam/bear-dataset)
-
 ### Objetivo:
-O objetivo é alimentar o modelo com imagens de ursos polares e negros, em seguida, apresentar imagens de ursos pandas para que o modelo tente classificar.
+O objetivo é ncontrar a linha sigmoidal que melhor se ajusta aos dados fornecidos a partir do algoritmo de gradiente descendente.
 
 ### Modelo:
-Regressão logística de 2º grau 
+Regressão logística de 2º grau e gradiente descendente
 
 ### Execução:
 ```bash
